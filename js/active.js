5299a9fedfd44731e7c3e76ccd0890d2601f064a
ripon630371 <riponcmt630371@gmail.com>
2018-06-11 02:31:57 +0600

    project area section ok

--- js/active.js
+++ js/active.js
@@ -19,9 +19,32 @@ $(document).ready(function(){
 	            items:1
 	        }
 	    }
-	})
+	});
 	//skillbar section plagins....
 	$('.skillbar').skillBars({
 	  // options here
 	});
+
+	//project-area owl-carousel...
+	$('.project-active').owlCarousel({
+	    loop:true,
+	    nav:true,
+	    autoplay:true,
+	    navText: ['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+	    autoplayTimeout:3000,
+	    smartSpeed:2000,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:4
+	        }
+	    }
+	})
+
+	
 })
