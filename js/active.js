d5015eb635f597de791f4b56671658372bb520f0
ripon630371 <riponcmt630371@gmail.com>
2018-06-12 02:28:56 +0600

    blog area right and left ok

--- js/active.js
+++ js/active.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+(function ($) {
 
 	//slider-area owl-carousel...
 	$('.slider-active').owlCarousel({
@@ -45,11 +45,30 @@ $(document).ready(function(){
 	        }
 	    }
 	});
+	//project-area owl-carousel...
+	$('.blog-active').owlCarousel({
+	    loop:true,
+	    nav:true,
+	    autoplay:true,
+	    navText: ['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+	    autoplayTimeout:3000,
+	    smartSpeed:2000,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        }
+	    }
+	});
 	// magnificPopup
     $('.popup-video').magnificPopup({
         type: 'iframe'
         // other options
     });
 
-	
-})
+})(jQuery);	
